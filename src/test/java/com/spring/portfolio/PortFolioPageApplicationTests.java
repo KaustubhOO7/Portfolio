999bda3a1a36247f7c7a0c775dd81a509b00b360
Kaustubh Sharma <sharma.kaustubh.05@gmail.com>
2024-07-08 21:37:02 +0530

    gif to png done

---
+++ src/test/java/com/spring/portfolio/PortFolioPageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortFolioPageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
